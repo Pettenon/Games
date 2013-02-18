0ee9a5e5cd79e9bbf54f6ba2c88cfa2f5e1cd7db
VictorPettenon <pettenon.r3cotia@gmail.com>
2013-02-18 11:39:58 -0300

    Games Formtastic

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   attr_accessible :category_id, :name, :plataform, :release
+
+  #belongs_to :category
+  #belongs_to :plataform
 end
--- app/models/plataform.rb
+++ app/models/plataform.rb
@@ -1,3 +1,5 @@
 class Plataform < ActiveRecord::Base
   attr_accessible :name
+
+  #has_many :games
 end
